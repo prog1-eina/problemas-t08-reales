9a120805838de2eb1df586ad180f5129b7b925ba
Miguel Ángel Latre Abadía <latre@unizar.es>
2023-10-08 21:19:13 +0200

    ``mingw32-make`` --> ``make``

--- 1-2-ecuacion-segundo-grado.cpp
+++ 1-2-ecuacion-segundo-grado.cpp
@@ -6,15 +6,15 @@
  *          ecuación tiene soluciones reales o no, y, en caso de tenerlas,
  *          también escribe dichas soluciones.
  *          Clase de problemas de Programación 1 del tema 8 (reales).
- * Nota: El programa completo está contenido en este fichero, por lo que puede 
- *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
- *       Code.
- *       También puede compilarse desde la terminal través de la orden
- *           g++ -Wall -Wextra 1-2-ecuacion-segundo-grado.cpp -o 1-2-ec
- *       y ejecutarse en Windows a través de la orden
- *           .\1-2-ec.exe
- *       o en Linux y macOS
- *           ./1-2-ec
+ * Nota: El programa completo está contenido en este fichero, por lo que puede 
+ *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
+ *       Code.
+ *       También puede compilarse desde la terminal través de la orden
+ *           g++ -Wall -Wextra 1-2-ecuacion-segundo-grado.cpp -o 1-2-ec
+ *       y ejecutarse en Windows a través de la orden
+ *           .\1-2-ec.exe
+ *       o en Linux y macOS
+ *           ./1-2-ec
 \******************************************************************************/
 #include <iostream>
 #include <cmath>
--- 4-media.cpp
+++ 4-media.cpp
@@ -6,15 +6,15 @@
  *          de los datos leídos de teclado, excluido el 0 que finaliza la
  *          secuencia.
  *          Clase de problemas de Programación 1 del tema 8 (reales).
- * Nota: El programa completo está contenido en este fichero, por lo que puede 
- *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
- *       Code.
- *       También puede compilarse desde la terminal través de la orden
- *           g++ -Wall -Wextra 4-media.cpp -o 4-media
- *       y ejecutarse en Windows a través de la orden
- *           .\4-media.exe
- *       o en Linux y macOS
- *           ./4-media
+ * Nota: El programa completo está contenido en este fichero, por lo que puede 
+ *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
+ *       Code.
+ *       También puede compilarse desde la terminal través de la orden
+ *           g++ -Wall -Wextra 4-media.cpp -o 4-media
+ *       y ejecutarse en Windows a través de la orden
+ *           .\4-media.exe
+ *       o en Linux y macOS
+ *           ./4-media
 \******************************************************************************/
 #include <iostream>
 #include <iomanip>
